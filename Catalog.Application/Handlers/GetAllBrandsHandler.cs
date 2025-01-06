22025278e62a9d294dca83b77fb0dba287d51899
Keith <karirukeith@gmail.com>
2025-01-06 22:02:35 +0300

    Added brand response, mapping profiles and mapper as well as the Handler for brand in the application layer

---
+++ Catalog.Application/Handlers/GetAllBrandsHandler.cs
@@ -0,0 +1,30 @@
+using AutoMapper;
+using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers;
+
+public class GetAllBrandsHandler: IRequestHandler<GetAllBrandsQuery, IList<BrandResponse>>
+{
+    private readonly IBrandRepository _brandRepository;
+
+    public GetAllBrandsHandler(IBrandRepository brandRepository)
+    {
+        _brandRepository = brandRepository;
+    }
+    
+    
+    public async Task<IList<BrandResponse>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+    {
+        IEnumerable<ProductBrand> brandList = await _brandRepository.GetAllBrands();
+
+        var brandResponseList = ProductMapper.Mapper.Map<IList<ProductBrand>, IList<BrandResponse>>(brandList.ToList());
+
+        return brandResponseList;
+
+    }
+}
---
+++ Catalog.Application/Mappers/ProductMappingProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+
+namespace Catalog.Application.Mappers;
+
+public class ProductMappingProfile: Profile
+{
+
+    public ProductMappingProfile()
+    {
+        CreateMap<ProductBrand, BrandResponse>().ReverseMap();
+    }
+    
+    
+}
