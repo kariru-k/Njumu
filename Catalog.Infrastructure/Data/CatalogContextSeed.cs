83c13ad6e68ab835e0ff108e0d9d1d202fe29efd
Keith <karirukeith@gmail.com>
2025-01-06 14:09:33 +0300

    Add ContextSeed classes to seed data to mongodb

---
+++ Catalog.Infrastructure/Data/CatalogContextSeed.cs
@@ -0,0 +1,28 @@
+using System.Text.Json;
+using Catalog.Core.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Infrastructure.Data;
+
+public static class CatalogContextSeed
+{
+    public static void SeedData(IMongoCollection<Product> catalogCollection)
+    {
+        bool checkCatalog = catalogCollection.CountDocuments(FilterDefinition<Product>.Empty) > 0;
+        string path = Path.Combine("Data", "SeedData", "products.json");
+
+        if (!checkCatalog)
+        {
+            var catalogData = File.ReadAllText(path);
+            var catalog = JsonSerializer.Deserialize<List<Product>>(catalogData);
+
+            if (catalog != null)
+            {
+                foreach (var item in catalog)
+                {
+                    catalogCollection.InsertOneAsync(item);
+                }
+            }
+        }
+    }
+}
