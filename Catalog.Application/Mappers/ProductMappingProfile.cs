2626b1f9537879d674d5563b2c34412c1e48f4d4
Keith <karirukeith@gmail.com>
2025-01-08 14:58:54 +0300

    Implemented create Product command and its handler as well as mapping between command, entity and response

--- Catalog.Application/Mappers/ProductMappingProfile.cs
+++ Catalog.Application/Mappers/ProductMappingProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using Catalog.Application.Commands;
 using Catalog.Application.Responses;
 using Catalog.Core.Entities;
 
@@ -12,6 +13,7 @@ public class ProductMappingProfile: Profile
         CreateMap<ProductBrand, BrandResponse>().ReverseMap();
         CreateMap<Product, ProductResponse>().ReverseMap();
         CreateMap<ProductType, TypeResponse>().ReverseMap();
+        CreateMap<Product, CreateProductCommand>().ReverseMap();
     }
     
     
