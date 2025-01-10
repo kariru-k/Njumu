ca8ca8c3c3d5c8061e8c6e9ed8a2d1740ce9a9cd
Keith <karirukeith@gmail.com>
2025-01-10 17:56:52 +0300

    Added catalog specification parameters for APIs

---
+++ Catalog.Core/Specs/CatalogSpecParams.cs
@@ -0,0 +1,23 @@
+namespace Catalog.Core.Specs;
+
+public class CatalogSpecParams
+{
+    private int _pageSize;
+    private const int MaxPageSize = 70;
+
+    public int PageIndex { get; set; } = 1;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+    }
+    
+    public string? BrandId { get; set; }
+    public string? TypeId { get; set; }
+    
+    public string? Sort { get; set; }
+    
+    private string? Search { get; set; }
+
+}
