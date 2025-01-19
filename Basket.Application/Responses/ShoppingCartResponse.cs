b3a09cf34f6ed6b35b8f850ebe254253e2c653f2
Keith <karirukeith@gmail.com>
2025-01-19 14:57:44 +0300

    Created CQRS responses

---
+++ Basket.Application/Responses/ShoppingCartResponse.cs
@@ -0,0 +1,27 @@
+namespace Basket.Application.Responses;
+
+public class ShoppingCartResponse
+{
+    public string UserName { get; set; }
+    
+    public List<ShoppingCartItemResponse> Items { get; set; } = new List<ShoppingCartItemResponse>();
+
+    public ShoppingCartResponse()
+    {
+        
+    }
+
+    public ShoppingCartResponse(string userName)
+    {
+        UserName = userName;
+    }
+    
+    public decimal TotalPrice
+    {
+        get
+        {
+            return Items.Sum(x => x.Price * x.Quantity);
+        }
+    }
+    
+}
