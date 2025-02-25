f21fdfd2a2d2e6d58343de920e5cc4ecafd4b714
Keith <karirukeith@gmail.com>
2025-02-25 09:50:32 +0300

    Refactoring

--- Catalog.Application/Queries/GetAllBrandsQuery.cs
+++
@@ -1,8 +0,0 @@
-using Catalog.Application.Responses;
-using MediatR;
-
-namespace Catalog.Application.Queries;
-
-public class GetAllBrandsQuery : IRequest<IList<BrandResponse>>
-{
-}
--- Catalog.Application/Responses/BrandResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace Catalog.Application.Responses;
-
-public class BrandResponse
-{
-    public string Id { get; set; }
-
-    public string Name { get; set; }
-}
